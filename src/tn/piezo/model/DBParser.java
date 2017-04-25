1ec869e38c11f5a17c1583a8105d06997e36f5e6
AzamatIlyasov <azamat3.1415@gmail.com>
2017-04-25 17:08:12 +0600

    sql-dbPiezo -- подключение к БД и считывание с таблцицы в БД (inputSampleTable) --

--- src/tn/piezo/model/DBParser.java
+++ src/tn/piezo/model/DBParser.java
@@ -1,8 +1,15 @@
 package tn.piezo.model;
 
+import javax.sql.RowSet;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by djaza on 08.02.2017.
@@ -24,142 +31,56 @@ public class DBParser {
     private static ArrayList<ArrayList> piezoData = new ArrayList<ArrayList>();
 
     //считывание с базы данных DBPiezo (MSSql server - AZAPCSQLEXPRESS)
-    /*
+
     public static ArrayList parseHydraT(String fileName)
     {
-        // инициализируем потоки
-        String result = "";
-        InputStream inputStream = null;
-        HSSFWorkbook workbook = null;
+        //данные для подключения к БД
+        String serverName = "AZAMATPC\\AZAPCSQLEXPRESS";
+        String dbName = "DBPiezo";
+        String userName = "sa";
+        String userPassword = "Server510";
+        String connectionUrl = "jdbc:sqlserver://%1$s;databaseName=%2$s;user=%3$s;password=%4$s;";
+        String connectionString = String.format(connectionUrl, serverName, dbName, userName, userPassword);
+        //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        //подключаемся к БД
         try {
-            inputStream = new FileInputStream(fileName);
-            workbook = new HSSFWorkbook(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //разбираем первый лист входного файла на объектную модель
-        Sheet sheet = workbook.getSheetAt(0);
-        Iterator<Row> itRforN = sheet.iterator();
-        itRforN.next();
-
-        int i = 0, n = 0;
-        //проходим по всему листу для получения кол-ва строк
-        String cellTestN = "", rowTestN = "";
-
-        while (itRforN.hasNext()) {
-            Row rowForN = itRforN.next();
-            Iterator<Cell> cellsForN = rowForN.iterator();
-            do{
-                Cell cellForN = cellsForN.next();
-                //перебираем возможные типы ячеек
-                switch (cellForN.getCellType()) {
-                    case Cell.CELL_TYPE_STRING:
-                        cellTestN = cellForN.getStringCellValue();
-                        break;
-                    case Cell.CELL_TYPE_NUMERIC:
-                        cellTestN = Double.toString(cellForN.getNumericCellValue());
-                        break;
-                    default:
-                        if (cellsForN.hasNext())
-                            cellTestN = "error"; //обозначения конца файла
-                        else cellTestN = "error2"; //обозначение конца строки
-                        break;
-                }
-                //проверка на конец строки
-                if (!cellsForN.hasNext()) break;
-
-                Cell cellForN2 = cellsForN.next();
-                //перебираем возможные типы ячеек
-                switch (cellForN2.getCellType()) {
-                    case Cell.CELL_TYPE_STRING:
-                        rowTestN = cellForN2.getStringCellValue();
-                        break;
-                    case Cell.CELL_TYPE_NUMERIC:
-                        rowTestN = Double.toString(cellForN2.getNumericCellValue());
-                        break;
-                    default:
-                        if (cellsForN.hasNext())
-                            rowTestN = "error";
-                        else rowTestN = "error2";
-                        break;
-                }
-
-                if ((rowTestN=="error") && (cellTestN=="error"))
-                    break;
-            } while(cellsForN.hasNext());
-
-            //увеличить кол-во участков
-            n++;
-            //проверка - закончились участки?, т.е. конец файла
-            if ((rowTestN=="error") && (cellTestN=="error")) {
-                n--;
-                break;
+            Connection con = DriverManager.getConnection(connectionString);
+            Statement stmt = con.createStatement();
+            //выборка необходимых данных
+            ResultSet executeQuery = stmt.executeQuery("SELECT num_rasch_Uch, num_pred_Uch, Diametr_Uch, " +
+                    "Length_Uch, G_Uch, Kekv_Uch, Geo_Uch, ZdanEtaj_Uch FROM [dbo].[inputSampleTable]");
+            //кол-во строк-участков
+            int n = executeQuery.getRow();
+            //инициализация массивов для исх данных
+            NamePartTNras = new String[n];
+            NamePartTNpred = new String[n];
+            D = new double[n];
+            L = new double[n];
+            G = new double[n];
+            Kekv = new double[n];
+            Geo = new double[n];
+            ZdanieEtaj = new double[n];
+            int i = 0;
+            //проходимся по всем строкам запроса
+            while (executeQuery.next()) {
+                NamePartTNras[i] = executeQuery.getString("num_rasch_Uch");
+                NamePartTNpred[i] = executeQuery.getString("num_pred_Uch");
+                D[i] = executeQuery.getDouble("Diametr_Uch");
+                L[i] = executeQuery.getDouble("Length_Uch");
+                G[i] = executeQuery.getDouble("G_Uch");
+                Kekv[i] = executeQuery.getDouble("Kekv_Uch");
+                Geo[i] = executeQuery.getDouble("Geo_Uch");
+                ZdanieEtaj[i] = executeQuery.getDouble("ZdanEtaj_Uch");
+                //следующая строка - участок
+                i++;
             }
-            //продолжаем считать
 
         }
-        //инициализация массивов для исх данных
-        NamePartTNras = new String[n];
-        NamePartTNpred = new String[n];
-        D = new double[n];
-        L = new double[n];
-        G = new double[n];
-        Kekv = new double[n];
-        Geo = new double[n];
-        ZdanieEtaj = new double[n];
-        Iterator<Row> itR = sheet.iterator();
-        itR.next(); // начинаем считывать со второй строки листа excel (первая заголовки)
-        //проходим по всему листу
-        while (itR.hasNext()) {
-            Row row = itR.next();
-            Iterator<Cell> cells = row.iterator();
-            //сохраняем наз текущего участка
-            Cell cell = cells.next();
-            if (!cells.hasNext()) break;
-            // т.к. наз могут служить и цифры
-            //перебираем возможные типы ячеек
-            switch (cell.getCellType()) {
-                case Cell.CELL_TYPE_STRING:
-                    NamePartTNras[i] = cell.getStringCellValue();
-                    break;
-                case Cell.CELL_TYPE_NUMERIC:
-                    NamePartTNras[i] = Double.toString(cell.getNumericCellValue());
-                    break;
-                default:
-                    NamePartTNras[i] = "error";
-                    break;
-            }
-            //сохраняем наз пред участка
-            Cell cell2 = cells.next();
-            // еще раз перебираем возможные типы ячеек
-            switch (cell2.getCellType()) {
-                case Cell.CELL_TYPE_STRING:
-                    NamePartTNpred[i] = cell2.getStringCellValue();
-                    break;
-                case Cell.CELL_TYPE_NUMERIC:
-                    NamePartTNpred[i] = Double.toString(cell2.getNumericCellValue());
-                    break;
-                default:
-                    NamePartTNpred[i] = "error";
-                    break;
-            }
-            //сохр D,L,G,Kekv,Geo,ZdanieEtaj
-            Cell cell3 = cells.next();
-            D[i] = cell3.getNumericCellValue();
-            Cell cell4 = cells.next();
-            L[i] = cell4.getNumericCellValue();
-            Cell cell5 = cells.next();
-            G[i] = cell5.getNumericCellValue();
-            Cell cell6 = cells.next();
-            Kekv[i] = cell6.getNumericCellValue();
-            Cell cell7 = cells.next();
-            Geo[i] = cell7.getNumericCellValue();
-            Cell cell8 = cells.next();
-            ZdanieEtaj[i] = cell8.getNumericCellValue();
-
-            i++; //следующий участок
-            if (i >= n) break; //участки закончились - выход за пределы массива
+        catch (SQLException sqlE) {
+            //логируем исключения
+            Logger.getLogger(DBParser.class.getName()).log(Level.SEVERE, null, sqlE);
         }
+
         Hrasp_ist = 90.0; // пока пост величина. в дальнейшем считывать интерфейса
 
         // ГР
@@ -169,7 +90,7 @@ public class DBParser {
         //выход их функции
         return hydraData;
     }
-
+/*
     //запись в файл Excel
     public static void writeExcelHydra(ArrayList HydraData , String fileName) {
         //создаем таблицу
@@ -217,6 +138,7 @@ public class DBParser {
 
     }
 */
+
     //расчет для ПГ
     public static ArrayList parsePiezoPlot(ArrayList HydraData)
     {
